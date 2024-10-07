82b319fa5255c328aff867d3496695ed259ee35d
Kolad2 <koladik@gmail.com>
2024-10-08 01:03:52 +0300

    update ModelGPU

--- main.py
+++ main.py
@@ -18,40 +18,25 @@ def modelini():
 	return model
 
 
-def image_cv2nn(img):
+def _image_cv2nn(img):
 	img = np.float32(img)
 	img = data_loader.prepare_image_cv2(img)
 	img = torch.unsqueeze(torch.from_numpy(img).cpu(), 0)
 	return img
 
 
-def image_nn2cv(img):
+def _image_nn2cv(img):
 	return torch.squeeze(img.detach()).cpu().numpy()
 
 
-def get_model_edges(model, img):
-	img_nn = image_cv2nn(img)
-	results = model(img_nn)
-	return image_nn2cv(results[-1])
-
-
-class modelgpu:
-	def image_cv2nn(self, img):
-		img = np.float32(img)
-		img = data_loader.prepare_image_cv2(img)
-		img = torch.unsqueeze(torch.from_numpy(img).cuda(), 0)
-		return img
-
-	def image_nn2cv(self, img):
-		return torch.squeeze(img.detach()).cpu().numpy()
-
-	def get_model_edges(self, img):
-		img_nn = self.image_cv2nn(img)
-		results = self.model(img_nn)
-		return self.image_nn2cv(results[-1])
-
+class ModelGPU:
 	def __init__(self):
 		path = os.path.dirname(os.path.realpath(__file__))
 		self.model = models.RCF()
 		self.model.cuda()
 		utils.load_pretrained(self.model, path + "/models/RCFcheckpoint_epoch12.pth", device='cuda')
+
+	def get_model_edges(self, img):
+		img_nn = _image_cv2nn(img)
+		results = self.model(img_nn)
+		return _image_nn2cv(results[-1])
