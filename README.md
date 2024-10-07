6eba5c2b4de6cd20db6e00a3e963d7d56181a8a0
Kolad2 <koladik@gmail.com>
2024-10-08 01:45:27 +0300

    update ModelGPU readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # rockedgesdetectors
 
 rcf model [download](https://drive.google.com/file/d/1ZY6W41xDJjG5jERd9aDHo6NJhu_H0EsW/view?usp=sharing)
+
+
+```python
+import cv2
+from rockedgesdetectors import ModelGPU
+
+big_frame = cv2.imread("image.png")
+model = ModelGPU("../models/RCFcheckpoint_epoch12.pth")
+edges = model.get_model_edges(big_frame)
+#
+cv2.namedWindow("wnd", cv2.WINDOW_NORMAL)
+cv2.resizeWindow('wnd', 800, 600)
+cv2.imshow("wnd", big_frame)
+cv2.waitKey(0)
+#
+cv2.namedWindow("wnd", cv2.WINDOW_NORMAL)
+cv2.resizeWindow('wnd', 800, 600)
+cv2.imshow("wnd", edges)
+cv2.waitKey(0)
+```
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
-import utils
-import models
-import data_loader
 import cv2
 import numpy as np
 from PIL import Image
 import torch
 from torch import nn
 import torchvision
+
+from rockedgesdetectors.pyrcf import utils, models, data_loader
 
 
 def modelini():
@@ -18,25 +17,34 @@ def modelini():
 	return model
 
 
-def _image_cv2nn(img):
+def _image_cv2cpunn(img):
 	img = np.float32(img)
 	img = data_loader.prepare_image_cv2(img)
 	img = torch.unsqueeze(torch.from_numpy(img).cpu(), 0)
 	return img
 
 
-def _image_nn2cv(img):
+def _image_cv2gpunn(img):
+	img = np.float32(img)
+	img = data_loader.prepare_image_cv2(img)
+	img = torch.unsqueeze(torch.from_numpy(img).cuda(), 0)
+	return img
+
+
+def _image_cpunn2cv(img):
 	return torch.squeeze(img.detach()).cpu().numpy()
 
 
 class ModelGPU:
-	def __init__(self):
-		path = os.path.dirname(os.path.realpath(__file__))
+	def __init__(self, path_to_model=None):
+		if path_to_model is None:
+			path = os.path.dirname(os.path.realpath(__file__))
+			path_to_model = path + "/models/RCFcheckpoint_epoch12.pth"
 		self.model = models.RCF()
 		self.model.cuda()
-		utils.load_pretrained(self.model, path + "/models/RCFcheckpoint_epoch12.pth", device='cuda')
+		utils.load_pretrained(self.model, path_to_model, device='cuda')
 
 	def get_model_edges(self, img):
-		img_nn = _image_cv2nn(img)
+		img_nn = _image_cv2gpunn(img)
 		results = self.model(img_nn)
-		return _image_nn2cv(results[-1])
+		return _image_cpunn2cv(results[-1])
