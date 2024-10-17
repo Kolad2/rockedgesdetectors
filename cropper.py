d6c1923621f580f0404e558dbf564c081ee9cb60
kolad <koladik@gmail.com>
2024-10-17 22:05:09 +0300

    module update

--- cropper.py
+++ cropper.py
@@ -1,42 +1,96 @@
 import math
 import numpy as np
 
+from scripts.edge_detect import result
+
 
 class Cropper:
 	def get_crop_edge(self, x, y, dx, dy, ddx, ddy):
 		img_small = self.img[y:y + dy, x:x + dx]
-		self.edges[y + ddy:y + dy - ddy, x + ddx:x + dx - ddx] = self.model(img_small)[ddy:dy - ddy, ddx:dx - ddx]
+		edges_small = self.model(img_small)
+		self.edges[y + ddy:y + dy - ddy, x + ddx:x + dx - ddx] = edges_small[ddy:dy - ddy, ddx:dx - ddx]
 
-	def get_cropped_edges(self, dx, dy, ddx, ddy):
-		i_max = math.floor((self.sh[0] - 2 * ddy) / (dy - 2 * ddy))
-		j_max = math.floor((self.sh[1] - 2 * ddx) / (dx - 2 * ddx))
+	def bottom_right_edges(self, dx, dy, ddx, ddy):
+		x, y = self.sh[1] - dx, self.sh[0] - dy
+		self.get_crop_edge(x, y, dx, dy, 0, 0)
+
+	def top_left_edges(self, dx, dy, ddx, ddy):
+		x, y = 0, 0
+		self.get_crop_edge(x, y, dx, dy, 0, 0)
+	
+	def bottom_left_edges(self, dx, dy, ddx, ddy):
+		x, y = 0, self.sh[0] - dy
+		self.get_crop_edge(x, y, dx, dy, 0, 0)
+
+	def top_right_edges(self, dx, dy, ddx, ddy):
+		x, y = self.sh[1] - dx, 0
+		self.get_crop_edge(x, y, dx, dy, 0, 0)
+
+	def right_edges(self, dx, dy, ddx, ddy):
+		step_y = dy - 2 * ddy
+		i_max = (self.sh[0] - 2 * ddy) // step_y
+		shift_y = (self.sh[0] - i_max * step_y) // 2 - ddy
+		x = self.sh[1] - dx
+		for i in range(0, i_max):
+			y = step_y * i + shift_y
+			self.get_crop_edge(x, y, dx, dy, 0, ddy)
+
+	def left_edges(self, dx, dy, ddx, ddy):
+		step_y = dy - 2 * ddy
+		i_max = (self.sh[0] - 2 * ddy) // step_y
+		shift_y = (self.sh[0] - i_max * step_y) // 2 - ddy
+		x = 0
+		for i in range(0, i_max):
+			y = step_y * i + shift_y
+			self.get_crop_edge(x, y, dx, dy, 0, ddy)
+
+	def bottom_edges(self, dx, dy, ddx, ddy):
+		step_x = dx - 2 * ddx
+		j_max = (self.sh[1] - 2 * ddx) // step_x
+		shift_x = (self.sh[1] - j_max * step_x) // 2 - ddx
+		y = self.sh[0] - dy
+		for j in range(0, j_max):
+			x = step_x * j + shift_x
+			self.get_crop_edge(x, y, dx, dy, ddx, 0)
+
+	def top_edges(self, dx, dy, ddx, ddy):
+		step_x = dx - 2 * ddx
+		j_max = (self.sh[1] - 2 * ddx) // step_x
+		shift_x = (self.sh[1] - j_max*step_x) // 2 - ddx
+		x, y = shift_x, 0
+		for j in range(0, j_max):
+			self.get_crop_edge(x, y, dx, dy, ddx, 0)
+			x += step_x
+	
+	def center_edges(self, dx, dy, ddx, ddy):
+		step_x = dx - 2 * ddx
+		step_y = dy - 2 * ddy
+		i_max = (self.sh[0] - 2 * ddy) // step_y
+		j_max = (self.sh[1] - 2 * ddx) // step_x
+		shift_x = (self.sh[1] - j_max * step_x) // 2 - ddx
+		shift_y = (self.sh[0] - i_max * step_y) // 2 - ddy
 		for i in range(0, i_max):
 			for j in range(0, j_max):
-				if (i*i_max + j + 1) % 1 == 0:
-					print(i*i_max + j + 1,"/",j_max*i_max)
-				x = (dx - 2 * ddx) * j
-				y = (dy - 2 * ddy) * i
+				if (i * i_max + j + 1) % 1 == 0:
+					print(i * i_max + j + 1, "/", j_max * i_max)
+				x = step_x * j + shift_x
+				y = step_y * i + shift_y
 				self.get_crop_edge(x, y, dx, dy, ddx, ddy)
 
-		_x = (dx - 2 * ddx) * j_max
-		_y = (dy - 2 * ddy) * i_max
-		_dx = self.sh[1] - _x
-		_dy = self.sh[0] - _y
-		for j in range(0, j_max):
-			x = (dx - 2 * ddx) * j
-			self.get_crop_edge(x, _y, dx, _dy, ddx, ddy)
-
-		for i in range(0, i_max):
-			y = (dy - 2 * ddy) * i
-			self.get_crop_edge(_x, y, _dx, dy, ddx, ddy)
-		self.get_crop_edge(_x, _y, _dx, _dy, ddx, ddy)
+	def get_cropped_edges(self, dx, dy, ddx, ddy):
+		self.top_left_edges(dx, dy, ddx, ddy)
+		self.bottom_right_edges(dx, dy, ddx, ddy)
+		self.bottom_left_edges(dx, dy, ddx, ddy)
+		self.top_right_edges(dx, dy, ddx, ddy)
+		self.top_edges(dx, dy, ddx, ddy)
+		self.right_edges(dx, dy, ddx, ddy)
+		self.bottom_edges(dx, dy, ddx, ddy)
+		self.left_edges(dx, dy, ddx, ddy)
+		self.center_edges(dx, dy, ddx, ddy)
 		return self.edges
 
-	def __init__(self, model, img=None, crop=512, pad=64):
-		self.img = img
-		self.sh = img.shape if img is not None else None
+	def __init__(self, model, crop=512, pad=64):
 		self.model = model
-		self.edges = np.zeros(img.shape[0:2], np.float32) if img is not None else None
 		self.crop = crop
 		self.pad = pad
 
@@ -44,4 +98,8 @@ class Cropper:
 		self.img = image
 		self.sh = image.shape
 		self.edges = np.zeros(image.shape[0:2], np.float32)
-		return self.get_cropped_edges(self.crop, self.crop, self.pad, self.pad)
+		ddx = self.pad
+		ddy = self.pad
+		dx = self.crop if self.crop >= self.sh[1] else self.sh[1] and ddx = 0
+		dy = self.crop if self.crop >= self.sh[0] else self.sh[0] and ddy = 0
+		return self.get_cropped_edges(dx, dy, ddx, ddy)
